b1825ac3aaba6e44c1db714087260a3d6367cc35
RiderX <119419985+sobanshoaiby@users.noreply.github.com>
2025-09-07 18:55:06 +0530

    patchCh8

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,4 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
-import { inter } from '@/app/ui/fonts';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
